e00855d06e42f4ae190e3ba73075df0bfffe8ad7
Mäkeli <joel.m.maenpaa@gmail.com>
2024-12-10 18:36:07 +0200

    day one done

--- src/DayOne.hs
+++ src/DayOne.hs
@@ -3,7 +3,7 @@ module DayOne (day_one) where
 import Prelude
 import Data.List.Split (splitOn)
 import Data.List (sort)
-
+import qualified Data.Map as Map
 
 calculate_distance :: Num a => a -> a -> a
 calculate_distance x y = abs value
@@ -33,9 +33,35 @@ sort_and_calculate_distance (xs, ys) = calculate_distances_sum $ calculate_dista
     x = sort xs
     y = sort ys
 
+create_map :: (Read a, Num a, Ord a) => [a] -> Map.Map a Int
+create_map xs = Map.fromList zip_list
+  where
+    zip_list = zip xs $ repeat 0
+
+update_map :: (Read k, Num k, Ord k) => k -> Map.Map k Int -> Map.Map k Int
+update_map = Map.update plus_one
+  where
+    plus_one = Just . (+) 1
+
+calculate_counts_map :: (Read k, Num k, Ord k) => [k] -> Map.Map k Int -> Map.Map k Int
+calculate_counts_map xs counts_map = foldl (flip update_map) counts_map xs
+
+calculate_similarity :: (Int, Int) -> Int
+calculate_similarity (x, y) = x * y
+
+calculate_similarity_score :: [(Int, Int)] -> Int
+calculate_similarity_score = sum . map calculate_similarity
+
 day_one :: IO ()
 day_one = do
   content <- readFile "inputs/d1.txt" :: IO String
-  let distances = unzip_to_lists content :: ([Int], [Int])
-  let distance_sum = sort_and_calculate_distance distances :: Int
+  let (xs, ys) = unzip_to_lists content :: ([Int], [Int])
+  let distance_sum = sort_and_calculate_distance (xs, ys) :: Int
+  print "PART I:"
   print distance_sum
+  let counts_map = create_map xs
+  let counts = Map.toList (calculate_counts_map ys counts_map)
+  let similarity_score = calculate_similarity_score counts
+  print "PART II:"
+  print similarity_score
+
